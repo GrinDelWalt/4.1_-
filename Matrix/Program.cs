5b14c4ea51487faf063d48387d364c5627e67bc4
Grindelwalt <Sulxj600s@gmail.com>
2020-05-24 00:12:18 +0300

    Добавьте файлы проекта.

---
+++ Matrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите поличество столбцов: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите поличество строк: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            var matrix = new int[n,a];
+
+            for (int i = 0; i < n; i++)
+            {
+
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal sealed class Program
+{
+   
+    
+        static void Main()
+        {
+            Random r = new Random();
+            Int32[] arr = Enumerable.Range(0, 10).Select(i => r.Next(0, 100)).ToArray();
+
+            Console.Write("Элементы массива: ");
+            arr.ToList().ForEach(i => Console.Write("{0} ", i));
+            Console.Write("\nДва самых больших числа: ");
+            arr.OrderByDescending(i => i).Take(2).ToList().ForEach(i => Console.Write("{0} ", i));
+            Console.Write("\nДва самых маленьких числа: ");
+            arr.OrderBy(i => i).Take(2).ToList().ForEach(i => Console.Write("{0} ", i));
+        }
+    }
+
